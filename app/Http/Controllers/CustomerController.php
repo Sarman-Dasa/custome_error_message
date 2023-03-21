02faa1e74774021819a64c9dd189ff46c1991f1a
Sarman-Dasa <sarmand@zignuts.com>
2023-03-21 11:30:07 +0530

    list and get data of Customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Traits\ListingApiTrait;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    use ListingApiTrait;
+
+    public function list(Request $request)
+    {
+        $this->ListingValidation();
+
+        $query = Customer::query();
+        $searchable_fields = ['name'];
+
+        $data = $this->filterSearchPagination($query,$searchable_fields);
+        return ok('data',[
+            'customerList'  =>  $data['query']->get(),
+            'count'     =>  $data['count'],
+        ]);      
+    }
+
+    public function get($id)
+    {
+        $shop = Customer::with('shop','city')->withCount('city AS No-Of-City')->findOrFail($id);
+        return ok('Shop Data',$shop);
+    }
 }
