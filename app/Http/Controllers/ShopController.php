fffee834d2e078a65e08a7fcb8eef88449d949ff
Sarman-Dasa <sarmand@zignuts.com>
2023-03-21 11:30:22 +0530

    list and get Shop data

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Shop;
+use App\Traits\ListingApiTrait;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+    use ListingApiTrait;
+
+    public function list(Request $request)
+    {
+        $this->ListingValidation();
+
+        $query = Shop::query();
+        $searchable_fields = ['name','mobile','email'];
+
+        $data = $this->filterSearchPagination($query,$searchable_fields);
+        return ok('data',[
+            'shopList'  =>  $data['query']->get(),
+            'count'     =>  $data['count'],
+        ]);      
+    }
+
+    public function get($id)
+    {
+        $shop = Shop::with('city','customers')->findOrFail($id);
+        return ok('Shop Data',$shop);
+    }
 }
