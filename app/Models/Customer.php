c6bd3379b3154291032b908a6bdfda1b584c0450
Sarman-Dasa <sarmand@zignuts.com>
2023-03-21 11:30:59 +0530

    add relationship

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    /**
+     * Customer-Shop Relation
+     */
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class,'shop_id','id');
+    }
+
+    /**
+     * Customer-city relation
+     */
+    public function city()
+    {
+        return $this->belongsTo(City::class,'city_id','id');
+    }
 }
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    /**
+     * One-To-Many
+     * Shop-Customer Relationship
+     */
+    public function customers()
+    {
+        return $this->hasMany(Customer::class,'shop_id','id');
+    }
+
+    /**
+     * Shop-City Relationship
+     */
+    public function city()
+    {
+        return $this->belongsTo(City::class,'city_id','id');
+    }
 }
